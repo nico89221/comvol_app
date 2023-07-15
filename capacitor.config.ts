399001b0b87582375d9e3191ae6ca4f2f3463592
nico89221 <nico89221@hotmail.com>
2023-07-15 16:54:17 -0300

    primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-mobile',
+  webDir: 'www',
+  server: {
+    androidScheme: 'http'
+  }
+};
+
+export default config;
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { App } from '@capacitor/app';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit{
+
+  constructor(private browser:InAppBrowser) {}
+  ngOnInit(): void {
+    const browserInstance = this.browser.create("https://stellar-bublanina-20e9ef.netlify.app/","_self","location=no,hidenavigationbuttons=true,hideurlbar=true,zoom=no"); 
+    browserInstance.on("exit").subscribe(evt=>{
+      App.exitApp();
+    });
+  }
+
+}
